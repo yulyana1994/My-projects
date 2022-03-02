be2c23c87ea558f6a70d1ce2c4b8272dd0bad8f9
Yulianna Shylzhenka <95912770+yulyana1994@users.noreply.github.com>
2022-03-02 16:56:48 +0300

    add new file

---
+++ tic-tac-toy/script.js
@@ -0,0 +1,55 @@
+const area = document.getElementById('area');
+let move = 0;
+let result = '';
+const contentWrapper = document.getElementById('content');
+const modalResult = document.getElementById('modal-result-wrapper');
+const overLay = document.getElementById('overLay');
+const btnClose = document.getElementById('btn-close');
+
+
+area.addEventListener('click', e =>{
+    if (e.target.className = 'box'){
+        move%2 ===0 ? e.target.innerHTML = 'X' : e.target.innerHTML = '0';
+        move++;
+        check();
+    }
+})
+
+const check = () => {
+    const boxes = document.getElementsByClassName('box');
+    const arr = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ];
+    for(i=0; i< arr.length; i++){
+        if(boxes[arr[i][0]].innerHTML == 'X' && boxes[arr[i][1]].innerHTML == 'X' && boxes[arr[i][2]].innerHTML == 'X'){
+            result = 'Победили Крестики';
+            prepearResult(result);
+        }else if (boxes[arr[i][0]].innerHTML == '0' && boxes[arr[i][1]].innerHTML == '0' && boxes[arr[i][2]].innerHTML == '0'){
+            result = 'Победили Нолики';
+            prepearResult(result);
+        }else if (move===9){
+            result = 'Ничья';
+            prepearResult(result);
+        }
+    }
+}
+
+const prepearResult = winner =>{
+    contentWrapper.innerHTML = `${winner}!`;
+    modalResult.style.display = 'block'
+}
+
+const closeModal = () =>{
+    modalResult.style.display = 'none'
+    location.reload();
+}
+
+overLay.addEventListener('click', closeModal);
+btnClose.addEventListener('click', closeModal);
